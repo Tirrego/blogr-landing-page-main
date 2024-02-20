a7f82f7d99ef5ead5b02c97948db9f82243a9e14
Tirrego <g.schmittel@gmx.de>
2024-02-20 10:07:35 +0100

    Mobile adjustment

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 let dropdowns = document.querySelectorAll(".expand");
+let menu = document.querySelector(".menu-icon");
+let nav = document.querySelector(".nav-desktop");
+let exit = document.querySelector(".exit")
 
 dropdowns.forEach(function (dropdown) {
   dropdown.addEventListener("click", function () {
@@ -9,3 +12,15 @@ dropdowns.forEach(function (dropdown) {
     arrow.classList.toggle("rotate");
   });
 });
+
+menu.addEventListener("click", () => {
+  nav.classList.toggle("show");
+  exit.classList.toggle("disable")
+  menu.classList.toggle("disable")
+})
+
+exit.addEventListener("click", () => {
+  nav.classList.toggle("show");
+  exit.classList.toggle("disable")
+  menu.classList.toggle("disable")
+})
